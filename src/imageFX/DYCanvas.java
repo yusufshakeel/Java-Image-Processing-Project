165088b1a7b81377980c62708df273b236be39df
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-02 20:33:43 +0530

    DYCanvas class created.

---
+++ src/imageFX/DYCanvas.java
@@ -0,0 +1,84 @@
+package imageFX;
+
+/**
+ * File: DYCanvas.java
+ * 
+ * Description:
+ * This file contains some random canvas creating method.
+ * 
+ * @author Yusuf Shakeel
+ * @version 1.0
+ * Date: 26-01-2014 sun
+ */
+public class DYCanvas {
+    
+    /**
+     * This method will generate an image of different shade out of the entered image.
+     * 
+     * @param img The image color to mix.
+     */
+    public static void colorMix(MyImage img){
+        
+        int maskSize = 3;   //The width of the mask.
+        
+        /**
+         * Buffered array of pixels holds the intermediate value of pixels that
+         * is multiplied with the mask to get the final value for the center
+         * pixel under the mask.
+         */
+        int buff[];
+        
+        
+        /** 
+         * This array will store the output of the median filter operation which will
+         * be later written back to the original image pixels.
+         */
+        int outputPixels[] = new int[img.getImageTotalPixels()];
+
+        /** Sharpen operation */
+        for(int y = 0; y < img.getImageHeight(); y++){
+            for(int x = 0; x < img.getImageWidth(); x++){
+                /** Fill buff array */
+                int i = 0;
+                buff = new int[9];
+                for(int r = y - (maskSize / 2); r <= y + (maskSize / 2); r++){
+                    for(int c = x - (maskSize / 2); c <= x + (maskSize / 2); c++){
+                        if(r < 0 || r == img.getImageHeight() || c < 0 || c == img.getImageWidth()){
+                            /** Some portion of the mask is outside the image. */
+                            int tr = r, tc = c;
+                            if(r < 0){
+                                tr = r+1;
+                            }else if(r == img.getImageHeight()){
+                                tr = r-1;
+                            }
+                            if(c < 0){
+                                tc = c+1;
+                            }else if(c == img.getImageWidth()){
+                                tc = c-1;
+                            }
+                            buff[i] = img.getPixel(tc, tr);
+                        }else{
+                            buff[i] = img.getPixel(c, r);
+                        }
+                        i++;
+                    }
+                }
+                
+                /** Sum of the image buff */
+                int sum = 0;
+                for(i = 0; i < 9; i++){
+                    sum += buff[i];
+                }
+                
+                /** Save result in outputPixels array. */
+                outputPixels[x+(y*img.getImageWidth())] = sum;
+            }
+        }
+        /** Write the output pixels to the image pixels */
+        for(int y = 0; y < img.getImageHeight(); y++){
+            for(int x = 0; x < img.getImageWidth(); x++){
+                img.setPixelToValue(x, y, outputPixels[x+(y*img.getImageWidth())]);
+            }
+        }
+    }
+}//DYCanvas class ends here
