cf6a52cccae092d48a325942827b708d61ac8e0d
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-12 01:10:32 +0530

    update - threshold
    threshold_AverageOfRGBValue()
    autoThreshold_usingRedValueOfPixels()
    autoThreshold_usingGreenValueOfPixels()
    autoThreshold_usingBlueValueOfPixels()

--- src/imageFX/ThresholdImage.java
+++ src/imageFX/ThresholdImage.java
@@ -20,10 +20,10 @@ public class ThresholdImage {
      * If thresholdValue > avgPixelValue then set Pixel (x,y) to BLACK
      * else set Pixel (x,y) to WHITE
      * 
-     * @param thresholdValue value to be compared with the average pixel value.
      * @param img the Image object passed on which thresholding is performed.
+     * @param thresholdValue value to be compared with the average pixel value.
      */
-    public static void grayscale_Simple(int thresholdValue, MyImage img){
+    public static void threshold_AverageOfRGBValue(MyImage img, int thresholdValue){
         for(int y = 0; y < img.getImageHeight(); y++){
             for(int x = 0; x < img.getImageWidth(); x++){
                 int r = img.getRed(x,y);
@@ -39,5 +39,234 @@ public class ThresholdImage {
         }
     }
     
+    /**
+     * This method will threshold the image. It will generate a Black&White image.
+     * This method will calculate the thresholdValue and
+     * If thresholdValue > redValueOfPixel then set Pixel (x,y) to BLACK
+     * else set Pixel (x,y) to WHITE.
+     * 
+     * @param img the Image object passed on which thresholding is performed.
+     */
+    public static void autoThreshold_usingRedValueOfPixels(MyImage img){
+        
+        /**
+         * thresholdValue will hold the final threshold value for the image.
+         * Initially thresholdValue = 127 [i.e. 255/2 = 127 (integer part)]
+         * 
+         * iThreshold will hold the intermediate threshold value during computation
+         * of final threshold value.
+         */
+        int thresholdValue = 127, iThreshold;
+        
+        /**
+         * sum1 holds the sum of red values less than thresholdValue.
+         * sum2 holds the sum of red values greater than or equal to the thresholdValue.
+         * count1 holds the number of pixels whose red value is less than thresholdValue.
+         * count2 holds the number of pixels whose red value is greater than or equal to thresholdValue.
+         */
+        int sum1, sum2, count1, count2;
+        
+        /**
+         * mean1 is equal to sum1/count1.
+         * mean2 is equal to sum2/count2.
+         */
+        int mean1, mean2;
+        
+        /** searching thresholdValue */
+        while(true){
+            sum1 = sum2 = count1 = count2 = 0;
+            for(int y = 0; y < img.getImageHeight(); y++){
+                for(int x = 0; x < img.getImageWidth(); x++){
+                    int r = img.getRed(x,y);
+                    if(r < thresholdValue){
+                        sum1 += r;
+                        count1++;
+                    }else{
+                        sum2 += r;
+                        count2++;
+                    }
+                }
+            }
+            /** calculating mean */
+            mean1 = (count1 > 0)?(int)(sum1/count1):0;
+            mean2 = (count2 > 0)?(int)(sum2/count2):0;
+            
+            /** calculating intermediate threshold */
+            iThreshold = (mean1 + mean2)/2;
+            
+            if(thresholdValue != iThreshold){
+                thresholdValue = iThreshold;
+            }else{
+                break;
+            }
+        }
+        
+        System.out.println(thresholdValue);
+        
+        /** performing thresholding on the image pixels */
+        for(int y = 0; y < img.getImageHeight(); y++){
+            for(int x = 0; x < img.getImageWidth(); x++){
+                int r = img.getRed(x,y);
+                if(thresholdValue > r){
+                    img.setPixel(x,y,255,255,255,255);  //set WHITE
+                }else{
+                    img.setPixel(x,y,255,0,0,0);  //set BLACK                    
+                }
+            }
+        }
+    }
     
+    /**
+     * This method will threshold the image. It will generate a Black&White image.
+     * This method will calculate the thresholdValue and
+     * If thresholdValue > greenValueOfPixel then set Pixel (x,y) to BLACK
+     * else set Pixel (x,y) to WHITE.
+     * 
+     * @param img the Image object passed on which thresholding is performed.
+     */
+    public static void autoThreshold_usingGreenValueOfPixels(MyImage img){
+        
+        /**
+         * thresholdValue will hold the final threshold value for the image.
+         * Initially thresholdValue = 127 [i.e. 255/2 = 127 (integer part)]
+         * 
+         * iThreshold will hold the intermediate threshold value during computation
+         * of final threshold value.
+         */
+        int thresholdValue = 127, iThreshold;
+        
+        /**
+         * sum1 holds the sum of red values less than thresholdValue.
+         * sum2 holds the sum of red values greater than or equal to the thresholdValue.
+         * count1 holds the number of pixels whose red value is less than thresholdValue.
+         * count2 holds the number of pixels whose red value is greater than or equal to thresholdValue.
+         */
+        int sum1, sum2, count1, count2;
+        
+        /**
+         * mean1 is equal to sum1/count1.
+         * mean2 is equal to sum2/count2.
+         */
+        int mean1, mean2;
+        
+        /** searching thresholdValue */
+        while(true){
+            sum1 = sum2 = count1 = count2 = 0;
+            for(int y = 0; y < img.getImageHeight(); y++){
+                for(int x = 0; x < img.getImageWidth(); x++){
+                    int g = img.getGreen(x,y);
+                    if(g < thresholdValue){
+                        sum1 += g;
+                        count1++;
+                    }else{
+                        sum2 += g;
+                        count2++;
+                    }
+                }
+            }
+            /** calculating mean */
+            mean1 = (count1 > 0)?(int)(sum1/count1):0;
+            mean2 = (count2 > 0)?(int)(sum2/count2):0;
+            
+            /** calculating intermediate threshold */
+            iThreshold = (mean1 + mean2)/2;
+            
+            if(thresholdValue != iThreshold){
+                thresholdValue = iThreshold;
+            }else{
+                break;
+            }
+        }
+        
+        System.out.println(thresholdValue);
+        
+        /** performing thresholding on the image pixels */
+        for(int y = 0; y < img.getImageHeight(); y++){
+            for(int x = 0; x < img.getImageWidth(); x++){
+                int g = img.getGreen(x,y);
+                if(thresholdValue > g){
+                    img.setPixel(x,y,255,255,255,255);  //set WHITE
+                }else{
+                    img.setPixel(x,y,255,0,0,0);  //set BLACK                    
+                }
+            }
+        }
+    }
+    
+    /**
+     * This method will threshold the image. It will generate a Black&White image.
+     * This method will calculate the thresholdValue and
+     * If thresholdValue > blueValueOfPixel then set Pixel (x,y) to BLACK
+     * else set Pixel (x,y) to WHITE.
+     * 
+     * @param img the Image object passed on which thresholding is performed.
+     */
+    public static void autoThreshold_usingBlueValueOfPixels(MyImage img){
+        
+        /**
+         * thresholdValue will hold the final threshold value for the image.
+         * Initially thresholdValue = 127 [i.e. 255/2 = 127 (integer part)]
+         * 
+         * iThreshold will hold the intermediate threshold value during computation
+         * of final threshold value.
+         */
+        int thresholdValue = 127, iThreshold;
+        
+        /**
+         * sum1 holds the sum of red values less than thresholdValue.
+         * sum2 holds the sum of red values greater than or equal to the thresholdValue.
+         * count1 holds the number of pixels whose red value is less than thresholdValue.
+         * count2 holds the number of pixels whose red value is greater than or equal to thresholdValue.
+         */
+        int sum1, sum2, count1, count2;
+        
+        /**
+         * mean1 is equal to sum1/count1.
+         * mean2 is equal to sum2/count2.
+         */
+        int mean1, mean2;
+        
+        /** searching thresholdValue */
+        while(true){
+            sum1 = sum2 = count1 = count2 = 0;
+            for(int y = 0; y < img.getImageHeight(); y++){
+                for(int x = 0; x < img.getImageWidth(); x++){
+                    int b = img.getBlue(x,y);
+                    if(b < thresholdValue){
+                        sum1 += b;
+                        count1++;
+                    }else{
+                        sum2 += b;
+                        count2++;
+                    }
+                }
+            }
+            /** calculating mean */
+            mean1 = (count1 > 0)?(int)(sum1/count1):0;
+            mean2 = (count2 > 0)?(int)(sum2/count2):0;
+            
+            /** calculating intermediate threshold */
+            iThreshold = (mean1 + mean2)/2;
+            
+            if(thresholdValue != iThreshold){
+                thresholdValue = iThreshold;
+            }else{
+                break;
+            }
+        }
+        
+        System.out.println(thresholdValue);
+        
+        /** performing thresholding on the image pixels */
+        for(int y = 0; y < img.getImageHeight(); y++){
+            for(int x = 0; x < img.getImageWidth(); x++){
+                int b = img.getBlue(x,y);
+                if(thresholdValue > b){
+                    img.setPixel(x,y,255,255,255,255);  //set WHITE
+                }else{
+                    img.setPixel(x,y,255,0,0,0);  //set BLACK                    
+                }
+            }
+        }
+    }
 }//class ThresholdImage ends here
