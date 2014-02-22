403c74e16b342ff075422546a0949c40ae26da87
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-22 20:29:32 +0530

    update

--- src/mypackage/ImageProject.java
+++ src/mypackage/ImageProject.java
@@ -21,18 +21,41 @@ public class ImageProject {
      */
     public static void main(String[] args){//throws IOException {
         test();
+        //RGB_HSI_test();
     }//main() ends here
     
     public static void test(){
         MyImage iobj = new MyImage();
-        
-        iobj.readImage("D:\\NoiseImage2.jpg");
+        iobj.readImage("D:\\Taj.jpg");
         iobj.initPixelArray();
-        FilterImage.medianFilter_RGB(iobj, 3);
-        //ImageFX.grayScale_Luminosity(iobj);
-        //ImageFX.greenImage(iobj);
-        //ThresholdImage.threshold_AverageOfRGBValue(iobj, 136);
-        //ThresholdImage.autoThreshold_usingBlueValueOfPixels(iobj);
-        iobj.writeImage("D:\\Taj2-threshold.jpg");
+        ImageFX.HSI_changeImageHueValue(iobj, 300);
+        //ImageFX.HSI_changeImageSaturationValue(iobj, 0.5);
+        //ImageFX.HSI_changeImageIntensityValue(iobj, 200);
+        iobj.writeImage("D:\\Taj2-HSI-hue.jpg");
+    }
+    
+    public static void RGB_HSI_test(){
+        MyImage iobj = new MyImage();
+        
+        iobj.readImage("D:\\testPixel.png");
+        iobj.initPixelArray();
+        System.out.println("before");
+        System.out.println(iobj.getRed(0, 0));
+        System.out.println(iobj.getGreen(0, 0));
+        System.out.println(iobj.getBlue(0, 0));
+        System.out.println(iobj.getHue_HSI(0, 0));
+        System.out.println(iobj.getSaturation_HSI(0, 0));
+        System.out.println(iobj.getIntensity_HSI(0, 0));
+        //ImageFX.HSI_changeImageHueValue(iobj, 300);
+        //ImageFX.HSI_changeImageSaturationValue(iobj, 0.5);
+        //ImageFX.HSI_changeImageIntensityValue(iobj, 100);
+        iobj.writeImage("D:\\testPixel2-hue.png");
+        System.out.println("after");
+        System.out.println(iobj.getRed(0, 0));
+        System.out.println(iobj.getGreen(0, 0));
+        System.out.println(iobj.getBlue(0, 0));
+        System.out.println(iobj.getHue_HSI(0, 0));
+        System.out.println(iobj.getSaturation_HSI(0, 0));
+        System.out.println(iobj.getIntensity_HSI(0, 0));
     }
 }//class ImageProject ends here
