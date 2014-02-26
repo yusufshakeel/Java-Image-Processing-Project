ceb5dfb0eff414018eea178696e98f8c6e5acaf8
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-26 16:43:50 +0530

    Initial - Grayscale.java

---
+++ example/Grayscale.java
@@ -0,0 +1,61 @@
+/**
+ * File: Grayscale.java
+ * 
+ * Description:
+ * Convert color image into grayscale image.
+ * 
+ * @author Yusuf Shakeel
+ * Date: 26-01-2014 sun
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class Grayscale{
+    public static void main(String args[])throws IOException{
+        BufferedImage img = null;
+        File f = null;
+        
+        //read image
+        try{
+            f = new File("D:\\Image\\Taj.jpg");
+            img = ImageIO.read(f);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        
+        //get image width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
+        
+        //convert to grayscale
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int p = img.getRGB(x,y);
+                
+                int a = (p>>24)&0xff;
+                int r = (p>>16)&0xff;
+                int g = (p>>8)&0xff;
+                int b = p&0xff;
+                
+                //calculate average
+                int avg = (r+g+b)/3;
+                
+                //replace RGB value with avg
+                p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+                
+                img.setRGB(x, y, p);
+            }
+        }
+        
+        //write image
+        try{
+            f = new File("D:\\Image\\Output.jpg");
+            ImageIO.write(img, "jpg", f);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }//main() ends here
+}//class ends here
