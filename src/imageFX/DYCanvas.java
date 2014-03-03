7f6a87e91572876b5762df4f90eb42b64cec3de1
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-03-03 21:33:33 +0530

    update - pictureText() added

--- src/imageFX/DYCanvas.java
+++ src/imageFX/DYCanvas.java
@@ -266,4 +266,40 @@ public class DYCanvas {
             }
         }
     }
+    
+    ///////////////////////////////// LAYERS ///////////////////////////////////
+    /**
+     * This method will replace the text area with the image that is placed beneath it.
+     * This function takes two images. One is the photo image and the other is the text
+     * image. The text image has a white background and a text message written in black color.
+     * The text image is placed on top of the photo image. This function will replace the
+     * text pixels of the text image with the photo pixels of the photo image that is
+     * beneath it.
+     * 
+     * @param testImg The image object that holds the text message image.
+     * @param photoImg The image object that holds the photo image.
+     */
+    public static void Layer_pictureText(MyImage textImg, MyImage photoImg){
+        for(int y = 0; y < textImg.getImageHeight(); y++){
+            for(int x = 0; x < textImg.getImageWidth(); x++){
+                //get text image RGB value
+                int r = textImg.getRed(x, y);
+                int g = textImg.getGreen(x, y);
+                int b = textImg.getBlue(x, y);
+                
+                //check whether pixel is not WHITE
+                if(r != 255 || g != 255 || b != 255){
+                    if((x+y*photoImg.getImageWidth())<= photoImg.getImageTotalPixels()){
+                        //photo image exists below text image. take photo image RGB value
+                        r = photoImg.getRed(x, y);
+                        g = photoImg.getGreen(x, y);
+                        b = photoImg.getBlue(x, y);
+
+                        //set photo image pixel value to text image pixel value
+                        textImg.setPixel(x, y, 255, r, g, b);
+                    }
+                }
+            }
+        }
+    }
 }//DYCanvas class ends here
