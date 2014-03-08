613be6b8cb3c2a5af5eeb4c33a3f82608ddb4f31
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-03-08 17:32:36 +0530

    update

--- src/mypackage/ImageProject.java
+++ src/mypackage/ImageProject.java
@@ -19,11 +19,22 @@ public class ImageProject {
      */
     public static void main(String[] args){
         test();
+        //photoText();
     }//main() ends here
     
     public static void test(){
-        MyImage iobj = new MyImage();
-        iobj.readImage("D:\\Taj.jpg");
+        MyImage iobj = new MyImage(3000,1500);
+        //iobj.readImage("D:\\Taj.jpg");
+        DYMosaic.myColorMosaic(iobj, DYColor.Ruby_red, 100);
         iobj.writeImage("D:\\Taj2-hsi.png");
+    }
+    
+    public static void photoText(){
+        MyImage textImg = new MyImage();
+        textImg.readImage("D:\\imgText3.png");
+        MyImage photoImg = new MyImage();
+        photoImg.readImage("D:\\Taj.jpg");
+        DYCanvas.Layer_pictureText(textImg, photoImg);
+        textImg.writeImage("D:\\Output.png");
     }
 }//class ImageProject ends here
