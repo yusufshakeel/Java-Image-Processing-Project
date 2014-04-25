02d431888142f1b2ce24ab558f312db95658a162
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-04-25 21:46:46 +0530

    Initial commit

---
+++ src/imageFX/morph/Erosion.java
@@ -0,0 +1,124 @@
+package imageFX.morph;
+import imageFX.MyImage;
+
+/**
+ * File: Erosion.java
+ * 
+ * Description:
+ * Implements the image morphology operation: erosion.
+ * 
+ * Erosion operation is basically applied on binary image. The basic effect is to erode away the
+ * boundaries of regions of foreground pixels (i.e., WHITE pixels, typically).
+ * Binary images are images whose pixels have only two possible intensity values - BLACK and WHITE.
+ * 
+ * WHITE is represented as (255,255,255,255) and BLACK as (255,0,0,0) in ARGB form respectively.
+ * 
+ * @author Yusuf Shakeel
+ * @version 1.0
+ * date: 24-042014 Thu
+ * 
+ * www.github.com/yusufshakeel/Java-Image-Processing-Project
+ * 
+ * The MIT License (MIT)
+ * Copyright (c) 2014 Yusuf Shakeel
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+public class Erosion {
+
+    /**
+     * This method will perform erosion operation on the binary image img.
+     * A binary image has two types of pixels - Black and White.
+     * WHITE pixel has the ARGB value (255,255,255,255)
+     * BLACK pixel has the ARGB value (255,0,0,0)
+     * 
+     * For erosion we generally consider foreground pixel. So, erodeBlackpixel = false
+     * 
+     * @param img The image on which erosion operation is performed
+     * @param erodeBlackPixel If set to TRUE will perform erosion on BLACK pixels else on WHITE pixels.
+     */
+    public static void binaryImage(MyImage img, boolean erodeBlackPixel){
+        /**
+         * Dimension of the image img.
+         */
+        int width = img.getImageWidth();
+        int height = img.getImageHeight();
+        
+        /**
+         * This will hold the erosion result which will be copied to image img.
+         */
+        int output[] = new int[width * height];
+        
+        /**
+         * If erosion is to be performed on BLACK pixels then
+         * targetValue = 0
+         * else
+         * targetValue = 255;  //for WHITE pixels
+         */
+        int targetValue = (erodeBlackPixel == true)?0:255;
+        
+        /**
+         * If the target pixel value is WHITE (255) then the reverse pixel value will
+         * be BLACK (0) and vice-versa.
+         */
+        int reverseValue = (targetValue == 255)?0:255;
+        
+        //perform erosion
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                //For BLACK pixel RGB all are set to 0 and for WHITE pixel all are set to 255.
+                if(img.getRed(x, y) == targetValue){
+                    /**
+                     * We are using a 3x3 kernel
+                     * [1, 1, 1
+                     *  1, 1, 1
+                     *  1, 1, 1]
+                     */
+                    boolean flag = false;   //this will be set if a pixel of reverse value is found in the mask
+                    for(int ty = y - 1; ty <= y + 1 && flag == false; ty++){
+                        for(int tx = x - 1; tx <= x + 1 && flag == false; tx++){
+                            if(ty >= 0 && ty < height && tx >= 0 && tx < width){
+                                //origin of the mask is on the image pixels
+                                if(img.getRed(tx, ty) != targetValue){
+                                    flag = true;
+                                    output[x+y*width] = reverseValue;
+                                }
+                            }
+                        }
+                    }
+                    if(flag == false){
+                        //all pixels inside the mask [i.e., kernel] were of targetValue
+                        output[x+y*width] = targetValue;
+                    }
+                }else{
+                    output[x+y*width] = reverseValue;
+                }
+            }
+        }
+        
+        /**
+         * Save the erosion value in image img.
+         */
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int v = output[x+y*width];
+                img.setPixel(x, y, 255, v, v, v);
+            }
+        }
+    }
+}//class ends here
