da6822609fe9e733162693c40fb7f7a77d5bc45c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-06 17:28:01 +0530

    added grayscaleImage()

--- src/imageFX/morph/Erosion.java
+++ src/imageFX/morph/Erosion.java
@@ -121,4 +121,56 @@ public class Erosion {
             }
         }
     }
+    
+    /**
+     * This method will perform erosion operation on the grayscale image img.
+     * 
+     * @param img The image on which erosion operation is performed
+     */
+    public static void grayscaleImage(MyImage img){
+        /**
+         * Dimension of the image img.
+         */
+        int width = img.getImageWidth();
+        int height = img.getImageHeight();
+        
+        //buff
+        int buff[];
+        
+        //output of erosion
+        int output[] = new int[width*height];
+        
+        //perform erosion
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                buff = new int[9];
+                int i = 0;
+                for(int ty = y - 1; ty <= y + 1; ty++){
+                   for(int tx = x - 1; tx <= x + 1; tx++){
+                       if(ty >= 0 && ty < height && tx >= 0 && tx < width){
+                           //pixel under the mask
+                           buff[i] = img.getRed(tx, ty);
+                           i++;
+                       }
+                   }
+                }
+                
+                //sort buff
+                java.util.Arrays.sort(buff);
+                
+                //save lowest value
+                output[x+y*width] = buff[9-i];
+            }
+        }
+        
+        /**
+         * Save the erosion value in image img.
+         */
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int v = output[x+y*width];
+                img.setPixel(x, y, 255, v, v, v);
+            }
+        }
+    }
 }//class ends here
