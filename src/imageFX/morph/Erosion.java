a16bd363af535a543bbd0f916b51039f97deaeea
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-07 18:41:56 +0530

    added grayscale image erosion with mask

--- src/imageFX/morph/Erosion.java
+++ src/imageFX/morph/Erosion.java
@@ -147,6 +147,12 @@ public class Erosion {
                 int i = 0;
                 for(int ty = y - 1; ty <= y + 1; ty++){
                    for(int tx = x - 1; tx <= x + 1; tx++){
+                       /**
+                        * 3x3 mask [kernel or structuring element]
+                        * [1, 1, 1
+                        *  1, 1, 1
+                        *  1, 1, 1]
+                        */
                        if(ty >= 0 && ty < height && tx >= 0 && tx < width){
                            //pixel under the mask
                            buff[i] = img.getRed(tx, ty);
@@ -160,6 +166,75 @@ public class Erosion {
                 
                 //save lowest value
                 output[x+y*width] = buff[9-i];
+            }
+        }
+        
+        /**
+         * Save the erosion value in image img.
+         */
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int v = output[x+y*width];
+                img.setPixel(x, y, 255, v, v, v);
+            }
+        }
+    }
+    
+    /**
+     * This method will perform erosion operation on the grayscale image img.
+     * It will find the minimum value among the pixels that are under the mask [element value 1] and will
+     * set the origin to the minimum value.
+     * 
+     * @param img The image on which erosion operation is performed
+     * @param mask the square mask.
+     * @param maskSize the size of the square mask. [i.e., number of rows]
+     */
+    public static void grayscaleImage(MyImage img, int mask[], int maskSize){
+        /**
+         * Dimension of the image img.
+         */
+        int width = img.getImageWidth();
+        int height = img.getImageHeight();
+        
+        //buff
+        int buff[];
+        
+        //output of erosion
+        int output[] = new int[width*height];
+        
+        //perform erosion
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                buff = new int[maskSize * maskSize];
+                int i = 0;
+                for(int ty = y - maskSize/2, mr = 0; ty <= y + maskSize/2; ty++, mr++){
+                   for(int tx = x - maskSize/2, mc = 0; tx <= x + maskSize/2; tx++, mc++){
+                       /**
+                        * Sample 3x3 mask [kernel or structuring element]
+                        * [0, 1, 0
+                        *  1, 1, 1
+                        *  0, 1, 0]
+                        * 
+                        * Only those pixels of the image img that are under the mask element 1 are considered.
+                        */
+                       if(ty >= 0 && ty < height && tx >= 0 && tx < width){
+                           //pixel under the mask
+                           
+                           if(mask[mc+mr*maskSize] != 1){
+                               continue;
+                           }
+                           
+                           buff[i] = img.getRed(tx, ty);
+                           i++;
+                       }
+                   }
+                }
+                
+                //sort buff
+                java.util.Arrays.sort(buff);
+                
+                //save lowest value
+                output[x+y*width] = buff[(maskSize*maskSize) - i];
             }
         }
         
