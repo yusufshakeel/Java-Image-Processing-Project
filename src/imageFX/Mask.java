f3447ca2317f7590dee4c10ad338d0bfa1e859ef
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-08 21:19:50 +0530

    added diamond shape mask

--- src/imageFX/Mask.java
+++ src/imageFX/Mask.java
@@ -104,6 +104,81 @@ public class Mask {
         return mask;
     }
     
+    //////////////////////////////// DIAMOND ///////////////////////////////////
+    
+    /**
+     * This will return a square mask having diamond shape set to 1.
+     * 
+     * @param width The width of the square mask. [An odd integer like 9, 11, 13 etc]
+     * @return 2D mask
+     */
+    public static int[] drawDiamond(int width){
+        int mask[] = new int[width * width];
+        int x = 0, y = width/2;
+        int cx = width/2, cy = width/2;
+        
+        while(x<=y){
+            //draw diamond
+            mask[(cx+x)+(cy+y)*width] = 1;
+            mask[(cx-x)+(cy+y)*width] = 1;
+            mask[(cx+x)+(cy-y)*width] = 1;
+            mask[(cx-x)+(cy-y)*width] = 1;
+            mask[(cx+y)+(cy+x)*width] = 1;
+            mask[(cx-y)+(cy+x)*width] = 1;
+            mask[(cx+y)+(cy-x)*width] = 1;
+            mask[(cx-y)+(cy-x)*width] = 1;
+            
+            x++;
+            y--;
+        }
+        return mask;
+    }
+    
+    /**
+     * This will return a square mask with filled diamond.
+     * 
+     * @param width The width of the square mask. [An odd integer like 9, 11, 13...]
+     * @return 2D mask
+     */
+    public static int[] fillDiamond(int width){
+        int cx = width/2, cy = width/2;
+        
+        //draw diamond
+        mask = drawDiamond(width);
+        
+        //fill diamond
+        floodFill(width, width, cx, cy, 0, 1);
+        
+        return mask;
+    }
+    
+    /**
+     * This will return a square mask with of diamond shape with corner filled with 1.
+     * 
+     * @param width The width of the square mask. [An odd integer like 9, 11, 13...]
+     * @return 2D mask
+     */
+    public static int[] fillDiamondCorners(int width){
+        int cx = width/2, cy = width/2;
+        
+        //draw diamond
+        mask = drawDiamond(width);
+        
+        //fill diamond
+        floodFill(width, width, cx, cy, 0, 1);
+        
+        //invert
+        for(int y = 0; y < width; y++){
+            for(int x = 0; x < width; x++){
+                mask[x+y*width] = (mask[x+y*width]==1)?0:1;
+            }
+        }
+        
+        return mask;
+    }
+    
+    ///////////////////////////// FLOOD FILL ///////////////////////////////////
+    
     /**
      * This method will floodfill the image.
      * 
